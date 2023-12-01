36e449429ad647f6984b2ade112edc3d0b9bac4a
kezakezake <d.pagliarini95@gmail.com>
2023-12-01 12:20:13 +0100

    funzionalità aggiunte, visualiazza, aggiungi e sconta

--- src/org/generation/italy/Negozio.java
+++ src/org/generation/italy/Negozio.java
@@ -1,30 +1,47 @@
 package org.generation.italy;
 
 import java.util.HashMap;
+import java.util.Scanner;
 
 public class Negozio {
 	private HashMap<Integer,Prodotto> elencoProdotti=new HashMap<>();
 
+
 	public Negozio() {
-		elencoProdotti.put(1, new Prodotto("jeans","JE001",30.0f));
-		elencoProdotti.put(2, new Prodotto("t-shirt","TS001",15.5f));
-		elencoProdotti.put(3, new Prodotto("scarpe","SC001",70.0f));
-		elencoProdotti.put(4, new Prodotto("jeans","JE002",35.0f));
+		elencoProdotti.put(1, new Prodotto("JEANS ANNA PEPE","JE001",30.0f));
+		elencoProdotti.put(2, new Prodotto("T-SHIRT ANNA PEPE","TS001",15.5f));
+		elencoProdotti.put(3, new Prodotto("SCARPE ANNA PEPE","SC001",70.0f));
+		elencoProdotti.put(4, new Prodotto("JEANS RAY MISTIRIO","JE002",35.0f));
 
 	}
 
-	public void aggiungiProdotto() {
+	public void aggiungiProdotto(String descrizione, String codiceProdotto, float prezzo ) {
 		
+		
+		elencoProdotti.put(elencoProdotti.size()+1, new Prodotto(descrizione, codiceProdotto,prezzo));
+	
 	}
 
 	public void elencaProdotti() {
 		for(Integer n:elencoProdotti.keySet())
 		{
-			System.out.println(n+" "+ elencoProdotti.get(n).getCodiceProdotto()+" "+elencoProdotti.get(n).getDescrizione()+" "+ elencoProdotti.get(n).getPrezzo());
+			System.out.println(n+" "+ elencoProdotti.get(n).getCodiceProdotto()+" "+elencoProdotti.get(n).getDescrizione()+" "+ elencoProdotti.get(n).getPrezzo()+ "€" + " -"+ elencoProdotti.get(n).getQuantitaDisponibile()+ " Disponibili " + elencoProdotti.get(n).getSconto()+ "%");
 		
 		}
 			
 	}
+	 public void applicaSconto(Integer sceltaProdotto, float scontoScelto) {
+		if(scontoScelto<90f && scontoScelto>5f)
+		{
+	    elencoProdotti.get(sceltaProdotto).setSconto(scontoScelto);
+		}
+		else 
+			System.out.println("sconto non applicabile");
+			
+	}
+	
+		
+	
 	
 	public HashMap<Integer, Prodotto> getElencoProdotti() {
 		return elencoProdotti;
